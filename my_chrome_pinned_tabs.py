6fd8cfb6b1fe30fc3f04137017ff102f28c15e8c
Timal Peramune <tperamun@uwo.ca>
2020-06-28 20:07:11 -0400

    added one for google chats as well

--- my_chrome_pinned_tabs.py
+++ my_chrome_pinned_tabs.py
@@ -7,13 +7,13 @@ import time
 
 MESSENGER_URL = "https://www.messenger.com"
 GOOGLE_CALENDAR_URL = "https://calendar.google.com/calendar/r/week"
-
+GOOGLE_CHATS_URL = "https://chat.google.com/"
 
 
 driver = webdriver.Chrome("./chromedriver")
+driver.maximize_window()
+
 driver.get(MESSENGER_URL)
-
-
 driver.find_element_by_id('email').send_keys(os.environ["MESSENGER_USERNAME"])
 driver.find_element_by_id('pass').send_keys(os.environ["MESSENGER_PASSWORD"])
 driver.find_element_by_id('loginbutton').click()
@@ -23,6 +23,17 @@ driver.find_element_by_id('loginbutton').click()
 google_calendar_tab =  '''window.open("{}","_blank");'''.format(GOOGLE_CALENDAR_URL)
 driver.execute_script(google_calendar_tab)
 driver.switch_to_window(driver.window_handles[1])
+driver.find_element_by_id('identifierId').send_keys(os.environ['WORK_GMAIL_USERNAME'])
+driver.find_element_by_id('identifierNext').click()
+driver.implicitly_wait(2)
+driver.find_element_by_name('password').send_keys(os.environ['WORK_GMAIL_PASSWORD'])
+driver.find_element_by_id('passwordNext').click()
+
+
+
+google_chats_tab = '''window.open("{}","_blank");'''.format(GOOGLE_CHATS_URL)
+driver.execute_script(google_chats_tab)
+driver.switch_to_window(driver.window_handles[2])
 driver.find_element_by_id('identifierId').send_keys(os.environ['WORK_GMAIL_USERNAME'])
 driver.find_element_by_id('identifierNext').click()
 driver.implicitly_wait(2)
