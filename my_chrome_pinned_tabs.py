8f7e692cf9509b7c0419429f33ad5817c410cf09
Timal Peramune <tperamun@uwo.ca>
2020-06-28 20:09:25 -0400

    added one for google chats as well

--- my_chrome_pinned_tabs.py
+++ my_chrome_pinned_tabs.py
@@ -8,6 +8,7 @@ import time
 MESSENGER_URL = "https://www.messenger.com"
 GOOGLE_CALENDAR_URL = "https://calendar.google.com/calendar/r/week"
 GOOGLE_CHATS_URL = "https://chat.google.com/"
+YOUTUBE_URL = "https://www.youtube.com/"
 
 
 driver = webdriver.Chrome("./chromedriver")
@@ -16,7 +17,7 @@ driver.maximize_window()
 driver.get(MESSENGER_URL)
 driver.find_element_by_id('email').send_keys(os.environ["MESSENGER_USERNAME"])
 driver.find_element_by_id('pass').send_keys(os.environ["MESSENGER_PASSWORD"])
-driver.find_element_by_id('loginbutton').click()
+# driver.find_element_by_id('loginbutton').click()
 
 
 
@@ -27,7 +28,7 @@ driver.find_element_by_id('identifierId').send_keys(os.environ['WORK_GMAIL_USERN
 driver.find_element_by_id('identifierNext').click()
 driver.implicitly_wait(2)
 driver.find_element_by_name('password').send_keys(os.environ['WORK_GMAIL_PASSWORD'])
-driver.find_element_by_id('passwordNext').click()
+# driver.find_element_by_id('passwordNext').click()
 
 
 
@@ -38,4 +39,9 @@ driver.find_element_by_id('identifierId').send_keys(os.environ['WORK_GMAIL_USERN
 driver.find_element_by_id('identifierNext').click()
 driver.implicitly_wait(2)
 driver.find_element_by_name('password').send_keys(os.environ['WORK_GMAIL_PASSWORD'])
-driver.find_element_by_id('passwordNext').click()
+# driver.find_element_by_id('passwordNext').click()
+
+
+youtube_tab = '''window.open("{}","_blank");'''.format(YOUTUBE_URL)
+driver.execute_script(youtube_tab)
+driver.switch_to_window(driver.window_handles[2])
